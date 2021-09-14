c258a3194cb84fa7e84be65b53450b66bd081d0d
deepikaproject <69418309+deepikaproject@users.noreply.github.com>
2021-09-14 05:36:40 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='data-time-extraction',
+    version='0.1.0',
+
+    description='',
+    long_description='',
+
+    url='https://www.test.com/',
+    author='deepika',
+    author_email='info@test.com',
+
+    license='proprietary',
+
+    classifiers=[
+
+    ],
+
+    packages=find_packages(),
+
+
+)
